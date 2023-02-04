9e3d3a05d0dec35e4024de809bbf6443e8001294
Jayming Liu <hengjayming@gmail.com>
2023-02-04 15:18:41 +0900

    plot line goes to all points

--- standardplot.py
+++ standardplot.py
@@ -7,21 +7,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def meanFun():
-    return
+def meanFun(list: list):
+    #return np.average(list)
+    x = 0
+    for i in list:
+        x += i
+    return x/len(list)
 
 def varianceFun():
+
     return
 
 def covarianceFun():
     return
 
+#r^2 = SSR/SST
 def coefficientsFun():
     return
 
+def regressionSlope(data: list, xMean: int, yMean: int):
+    numerator = 0
+    denominator = 0
+
+    for item in data:
+        numerator += (item[0]-xMean) * (item[1] - yMean)
+        denominator += (item[0]-xMean) * (item[0]-xMean)
+
+    #float division??? python wack
+    return numerator/denominator
+
+def regressionYIntercept(slope: int, xMean: int, yMean: int):
+    return yMean - slope*xMean
+
+
 def plot_regression(data, slope, yintercept):
 
-    x = np.linspace(0, 4, 100)  # Create a list of evenly-spaced numbers over the range
+    xList = []
+    for x in data:
+        xList.append(x[0])
+
+    x = np.linspace(np.min(xList), np.max(xList), 100)  # Create a list of evenly-spaced numbers over the range
     plt.plot(x, slope*x + yintercept)       # Plot the line
 
     for coords in data:
@@ -31,10 +56,24 @@ def plot_regression(data, slope, yintercept):
 
     plt.show()                   # Display the plot
 
-    
-exampleData = [[1,14],[3,24],[2,18],[1,17],[3,27]]
-slope = 5
-yintercept = 10
+
+exampleData = [[1,14],[3,24],[2,18],[1,17],[3,27],[1,3]]
+
+xList = []
+for item in exampleData:
+    xList.append(item[0])
+
+xAverage = meanFun(xList)
+
+yList = []
+for item in exampleData:
+    yList.append(item[1])
+
+yAverage = meanFun(yList)
+
+slope = regressionSlope(exampleData, xAverage, yAverage)
+yintercept = regressionYIntercept(slope, xAverage, yAverage)
+print('slope='+str(slope)+'\nyIntercept='+str(yintercept))
 plot_regression(exampleData, slope, yintercept)
 
 
